c9a0b679c7e929912c587d68b6adde3d3a6e9aca
gokhan <gokhanyahyatorba@gmail.com>
2022-08-18 19:30:08 +0300

    Init

---
+++ Runtime/RuntimeExample.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Deployer.Template
+{
+    
+    public class RuntimeExample : MonoBehaviour
+    {
+        
+        /// <summary>
+        /// Start is called on the frame when a script is enabled just before
+        /// any of the Update methods is called the first time.
+        /// </summary>
+        void Start()
+        {
+        }
+
+        /// <summary>
+        /// Update is called every frame, if the MonoBehaviour is enabled.
+        /// </summary>
+        void Update()
+        {
+        }
+    }
+}
